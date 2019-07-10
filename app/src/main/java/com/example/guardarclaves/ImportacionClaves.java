e13704f240ba8da2cc8727223e196d549b16f945
sora <52552792+diegoL1995@users.noreply.github.com>
2019-07-10 03:42:00 -0400

    se agregaron las referencias de los botones del activity importar datos

--- app/src/main/java/com/example/guardarclaves/ImportacionClaves.java
+++ app/src/main/java/com/example/guardarclaves/ImportacionClaves.java
@@ -3,12 +3,18 @@ package com.example.guardarclaves;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ImportacionClaves extends AppCompatActivity {
 
+    private Button btnVolver, btnImportar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_importacion_claves);
+
+        btnImportar = findViewById(R.id.btnImortar);
+        btnVolver = findViewById(R.id.btnVolver);
     }
+
 }
